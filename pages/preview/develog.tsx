f54c45d30596d3c08d3813369302b41cf5a9ed68
Chloe <gtalk.with.nani@gmail.com>
2023-11-08 20:00:11 +0900

    Preview mobile responsive

--- pages/preview/develog.tsx
+++ pages/preview/develog.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import AnchorLink from '@/components/AnchorLink';
 import { images } from '@/images';
 import styles from '@/styles/preview.module.sass';
@@ -8,7 +10,17 @@ const Icon = styled.i({
   background: `url(${images.misc.outlink}) no-repeat 50% 50%/contain`,
 });
 
+export function useDesktop() {
+  const [isDesktop, setIsDesktop] = useState(false);
+  const desktop = useMediaQuery({ query: '(min-width: 992px)' });
+  useEffect(() => {
+    setIsDesktop(desktop);
+  }, [desktop]);
+  return isDesktop;
+}
+
 const PreviewDevelog = () => {
+  const isDesktop = useDesktop();
   return (
     <div className={styles.preview}>
       <div className={styles.content}>
@@ -22,9 +34,19 @@ const PreviewDevelog = () => {
         </h2>
         <p>미리보기 Preview</p>
         <div className={styles.images}>
-          <Image src="/images/develog1.webp" alt="" width="614" height="1030" className={styles.left} />
-          <Image src="/images/develog2.webp" alt="" width="740" height="848" className={styles.center} />
-          <Image src="/images/develog3.webp" alt="" width="603" height="739" className={styles.right} />
+          {isDesktop ? (
+            <>
+              <Image src="/images/develog1.webp" alt="" width="614" height="1030" className={styles.left} />
+              <Image src="/images/develog2.webp" alt="" width="740" height="848" className={styles.center} />
+              <Image src="/images/develog3.webp" alt="" width="603" height="739" className={styles.right} />
+            </>
+          ) : (
+            <>
+              <Image src="/images/develog1a.webp" alt="" width="990" height="2145" />
+              <Image src="/images/develog2a.webp" alt="" width="990" height="2145" />
+              <Image src="/images/develog3a.webp" alt="" width="990" height="2145" />
+            </>
+          )}
         </div>
       </div>
     </div>
--- pages/preview/news.tsx
+++ pages/preview/news.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import AnchorLink from '@/components/AnchorLink';
 import { images } from '@/images';
 import styles from '@/styles/preview.module.sass';
@@ -8,7 +10,17 @@ const Icon = styled.i({
   background: `url(${images.misc.outlink}) no-repeat 50% 50%/contain`,
 });
 
+export function useDesktop() {
+  const [isDesktop, setIsDesktop] = useState(false);
+  const desktop = useMediaQuery({ query: '(min-width: 992px)' });
+  useEffect(() => {
+    setIsDesktop(desktop);
+  }, [desktop]);
+  return isDesktop;
+}
+
 const PreviewNews = () => {
+  const isDesktop = useDesktop();
   return (
     <div className={styles.preview}>
       <div className={styles.content}>
@@ -22,9 +34,19 @@ const PreviewNews = () => {
         </h2>
         <p>미리보기 Preview</p>
         <div className={styles.images}>
-          <Image src="/images/news1.webp" alt="" width="614" height="1030" className={styles.left} />
-          <Image src="/images/news2.webp" alt="" width="740" height="848" className={styles.center} />
-          <Image src="/images/news3.webp" alt="" width="603" height="739" className={styles.right} />
+          {isDesktop ? (
+            <>
+              <Image src="/images/news1.webp" alt="" width="614" height="1030" className={styles.left} />
+              <Image src="/images/news2.webp" alt="" width="740" height="848" className={styles.center} />
+              <Image src="/images/news3.webp" alt="" width="603" height="739" className={styles.right} />
+            </>
+          ) : (
+            <>
+              <Image src="/images/news1a.webp" alt="" width="990" height="2145" />
+              <Image src="/images/news2a.webp" alt="" width="990" height="2145" />
+              <Image src="/images/news3a.webp" alt="" width="990" height="2145" />
+            </>
+          )}
         </div>
       </div>
     </div>
