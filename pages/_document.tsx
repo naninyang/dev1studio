ae00a1d53fa5a7fd5a39ff9e87eff9c359dd82c5
Chloe <gtalk.with.nani@gmail.com>
2023-11-10 17:00:28 +0900

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,10 +14,10 @@ const AppDocument: React.FC<DocumentProps> & {
   return (
     <Html lang="ko-KR">
       <Head>
-        <link href="/favicon/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
-        <link href="/favicon/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
-        <link href="/favicon/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-        <link href="/favicon/favicon.ico" rel="shortcut icon" />
+        <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+        <link href="/favicon.ico" rel="shortcut icon" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="Black-translucent" />
         <meta name="format-detection" content="telephone=no" />
