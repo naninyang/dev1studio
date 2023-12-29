8f8639c1953ffabaab144939d7951afd5b561b25
Chloe <gtalk.with.nani@gmail.com>
2023-12-29 18:44:01 +0900

    Profile responsive fixed

--- pages/preview/dev1studio.tsx
+++ pages/preview/dev1studio.tsx
@@ -4,31 +4,47 @@ import { images } from '@/images';
 import { scrollRefs } from '@/components/ScrollLink';
 import AnchorLink from '@/components/AnchorLink';
 import Portfolio from '../profile/portfolio';
-import { rem } from '@/styles/designSystem';
+import { mq, rem } from '@/styles/designSystem';
 import styles from '@/styles/preview.module.sass';
 
 const PortfolioNormal = styled.i({
   background: `url(${images.profile.portfolio.english}) no-repeat 50% 50%/contain`,
-  width: rem(453),
-  height: rem(184),
+  width: rem(151),
+  height: rem(61),
+  [mq.minLarge]: {
+    width: rem(453),
+    height: rem(184),
+  },
 });
 
 const PortfolioHover = styled.i({
   background: `url(${images.profile.portfolio.korean}) no-repeat 0 50%/cover`,
-  width: rem(289),
-  height: rem(59),
+  width: rem(96),
+  height: rem(20),
+  [mq.minLarge]: {
+    width: rem(289),
+    height: rem(59),
+  },
 });
 
 const ResumeNormal = styled.i({
   background: `url(${images.profile.resume.english}) no-repeat 50% 50%/contain`,
-  width: rem(322),
-  height: rem(99),
+  width: rem(107),
+  height: rem(33),
+  [mq.minLarge]: {
+    width: rem(322),
+    height: rem(99),
+  },
 });
 
 const ResumeHover = styled.i({
   background: `url(${images.profile.resume.korean}) no-repeat 0 50%/cover`,
-  width: rem(162),
-  height: rem(59),
+  width: rem(54),
+  height: rem(20),
+  [mq.minLarge]: {
+    width: rem(162),
+    height: rem(59),
+  },
 });
 
 const PreviewDev1studio = () => {
