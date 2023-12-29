67d983a62834fcff31acbb18042a4d0fc65402d4
Chloe <gtalk.with.nani@gmail.com>
2023-12-29 19:11:01 +0900

    Update Portfolio

--- pages/profile/portfolio.tsx
+++ pages/profile/portfolio.tsx
@@ -574,7 +574,7 @@ const Portfolio: React.FC<{ onClose: () => void }> = ({ onClose }) => {
               </div>
               <div className="detail">
                 <dt>프로젝트명 및 기간</dt>
-                <dd>디비파이어 2022.12</dd>
+                <dd>비디파이어 2022.12</dd>
                 <dt>상세내용</dt>
                 <dd>PM, UX Design</dd>
               </div>
